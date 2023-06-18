8053e2d4b7303ef6ff81f908ee2c023bc224d90a
Bordiuzhan <bordyujan@gmail.com>
2023-06-18 02:29:27 +0200

    credential

--- config/auth.js
+++ config/auth.js
@@ -7,6 +7,8 @@ export const authConfig = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-   
+    Credentials({
+        
+    })
   ],
 };
