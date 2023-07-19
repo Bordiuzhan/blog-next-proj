7d74ff6b5f5e94e920373fc3675efb24fd6aab64
Bordiuzhan <bordyujan@gmail.com>
2023-07-19 19:45:14 +0200

    corrected css

--- app/add/page.js
+++ app/add/page.js
@@ -1,21 +1,18 @@
-import FormAddPosts from "@/components/FormAddPost";
-import { authConfig } from "@/config/auth";
-import { getServerSession } from "next-auth";
-
-
-
+import FormAddPosts from '@/components/FormAddPost';
+import { authConfig } from '@/config/auth';
+import { getServerSession } from 'next-auth';
 
 export default async function PageAddPost() {
-  const session = await getServerSession(authConfig)
+  const session = await getServerSession(authConfig);
 
   return (
     <div>
       <div className="img-hero add">
-      <div className="title-wrapper">
-      <h1 className="title-hero">Add Post</h1>
+        <div className="title-wrapper">
+          <h1 className="title-hero">Add Post</h1>
+        </div>
+        <FormAddPosts />
       </div>
-      </div>
-      <FormAddPosts></FormAddPosts>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
     <>
-      <div className="img-hero home"></div>
+      <div className="img-hero home">
+        <div className="home-text-container">
+          <p className="home-text">
+            Excepteur deserunt excepteur sint adipisicing voluptate veniam culpa
+            anim. Ad voluptate et mollit in. Eu sunt consequat anim tempor nisi
+            eu voluptate. Ea proident enim proident ad minim quis tempor
+            cupidatat sit consectetur. Ea enim quis ullamco occaecat proident
+            et. Exercitation voluptate officia elit quis Lorem nostrud labore
+            fugiat. Elit nostrud magna aute mollit Lorem. Occaecat occaecat sit
+            magna aliquip ipsum sunt. Anim minim officia est ex nostrud et.
+            Ullamco minim aliqua sit tempor occaecat. Irure do ex quis
+            reprehenderit consequat nulla fugiat reprehenderit non voluptate.
+            Qui proident fugiat dolore mollit minim. Non dolor tempor sint ipsum
+            anim voluptate. Tempor eu amet consequat mollit. Magna quis aliquip
+            fugiat et. Deserunt veniam elit est ea mollit sit. Amet culpa nulla
+            dolore eu tempor qui qui veniam.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
