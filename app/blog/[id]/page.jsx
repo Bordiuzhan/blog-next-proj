0d52b991d34e5ad9e40db2ae6d0e3aa81302bf2b
Bordiuzhan <bordyujan@gmail.com>
2023-10-10 20:17:54 +0200

    fix 5

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -4,33 +4,33 @@ import { Post } from '@/components/Post';
 
 const BASE_URL = process.env.BASE_URL;
 
-async function getData(id) {
-  const response = await fetch(`${BASE_URL}/api/posts/${id}`, {
-    next: {
-      revalidate: 60,
-    },
-  });
+// async function getData(id) {
+//   const response = await fetch(`${BASE_URL}/api/posts/${id}`, {
+//     next: {
+//       revalidate: 60,
+//     },
+//   });
 
-  return response.json();
-}
+//   return response.json();
+// }
 
-export async function generateMetadata({ params: { id } }) {
-  const post = await getData(id);
+// export async function generateMetadata({ params: { id } }) {
+//   const post = await getData(id);
 
-  return {
-    title: post.title,
-  };
-}
+//   return {
+//     title: post.title,
+//   };
+// }
 
 export default async function PostPage({ params: { id } }) {
-  const post = await getData(id);
+  // const post = await getData(id);
 
   return (
     <>
-      {/* <Link className="back-btn" key={'Go back'} href={'/blog'}>
+      <Link className="back-btn" key={'Go back'} href={'/blog'}>
         ⬅ Go back
-      </Link> */}
-      <Post post={post}></Post>
+      </Link>
+      {/* <Post post={post}></Post> */}
     </>
   );
 }
